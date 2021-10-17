7b51e66796a2863581efa5b995771023b5967c20
saruj <jomsaruj@sarujs-MacBook-Pro-2.local>
2021-10-18 01:04:42 +0700

    Add some features

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", function(request,response){
-    url = "https://api.waqi.info/feed/bangkok/?token=225d53a6a809699c600b2591a5d2eb0dc3492d42";
+app.get("/", function(req,res){
+    res.sendFile(__dirname + "/index.html")
+})
+
+app.post("/", function(req,res){
+    const cityname = req.body.city
+    url = "https://api.waqi.info/feed/"+ cityname +"/?token=demo ";
     https.get(url, function(response){
         response.on("data", function(data){
             const weatherData = JSON.parse(data)
-            console.log(weatherData)
+            const city = weatherData.data.city.name
+            const pm25 = weatherData.data.iaqi.pm25.v
+            const pm10 = weatherData.data.iaqi.pm10.v
+            const o3 = weatherData.data.iaqi.o3.v
+            const co = weatherData.data.iaqi.co.v
+            res.write("<p>City name : " + city + "<p>")
+            res.write("<p>pm25 : " + pm25+ "<p>")
+            res.write("<p>pm10 : " + pm10+ "<p>")
+            res.write("<p>o3 : " + o3+ "<p>")
+            res.write("<p>co : " + co+ "<p>")
+            res.send()
         })
     })
 })
